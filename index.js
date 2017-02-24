82bca55448d9fb37d536143ce580975ddc3c8b23
Philip Reichenberger <preichenberger@philcolabs.com>
2017-02-24 03:31:58 -0500

    added new analyzer and direction mentions

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
+var request = require('request');
 const Discord = require('discord.js');
 const Natural = require('natural');
 const Intersect = require('intersect');
 const http = require('http')
 const client = new Discord.Client();
 
-
 positivityPhrases = [
   'You can do it!',
   'Hang in there!',
   'Tomorrow is a brand new day!',
   'It\'s ok, I get nervous sometimes too!',
-  'Reach for the stars! - Akaya Kimuryu',
-  'If Donald Trump could do it, you can too!',
+  'Reach for the stars! - Akaya Kimuryu', 'If Donald Trump could do it, you can too!',
   'At least your mom thinks your cool!',
   'Cheer up, buttercup!',
   'Turn that frown upside down!',
@@ -28,9 +27,10 @@ sadWords = [ 'sad',
   'mad',
   'angry',
   'hate',
+  'no one loves me',
   'you suck',
   '\';\'',
-  'wahh',
+  'wah',
 ]
 
 function handler(req, res) {
@@ -45,7 +45,6 @@ function positivity() {
   return item;
 }
 
-
 var server = http.createServer(handler);
 var tokenizer = new Natural.WordPunctTokenizer();
 
@@ -54,20 +53,54 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
+  // Don't reply to self
+  if (msg.member.id == client.user.id) {
+    return
+  }
+
   var words = tokenizer.tokenize(msg.content);
 
   // cleaned words
   for (var i = 0; i < words.length; i++)
     words[i] = words[i].trim().toLowerCase();
 
+  // check for commands or direct mentions
   if (words[0] == '!' && words[1] == 'positive') {
     msg.reply(positivity());
+    return
+  }
+  if (msg.mentions.users.get(client.user.id) !== undefined ) {
+    msg.reply(positivity());
+    return
   }
 
+
+  // check for overrides
   if (Intersect(words, sadWords).length > 0) {
     msg.reply(positivity());
+    return
   }
+
+  // Analyze
+  var opts = {
+    url: 'http://text-processing.com/api/sentiment/',
+    json: true,
+    form: {
+      text: msg.content
+    }
+  }
+
+  request.post(opts, function optionalCallback(err, r, body) {
+      if (err) {
+        return console.error(err);
+      }
+      if (body.label == 'neg') {
+        msg.reply(positivity());
+      }
+  });
+
 });
+
 
 client.login(process.env['DISCORD_TOKEN']);
 server.listen(process.env['PORT']);
